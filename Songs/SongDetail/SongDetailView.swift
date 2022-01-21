f19a56898b77bffe14a4e052dd8bf874c1c6d327
Clement Dudit <clement.dudit@etudiant.univ-lr.fr>
2022-01-21 15:08:08 +0100

    add async image

--- Songs/SongDetail/SongDetailView.swift
+++ Songs/SongDetail/SongDetailView.swift
@@ -13,35 +13,41 @@ struct SongDetailView: View {
     
     var body: some View {
         NavigationView {
-            List {
-                Section(header: Text("Song")) {
-                    HStack {
-                        Text("Title")
-                        Spacer()
-                        Text(song.title ?? "")
+            VStack {
+                AsyncImage(url: song.coverURL)
+                    .frame(width:100, height:100)
+                    .scaledToFill()
+                    .cornerRadius(100)
+                List {
+                    Section(header: Text("Song")) {
+                        HStack {
+                            Text("Title")
+                            Spacer()
+                            Text(song.title ?? "")
+                        }
+                        HStack {
+                            Text("Release date")
+                            Spacer()
+                            Text(song.releaseDate ?? Date(), style: .date)
+                        }
+                        HStack {
+                            Text("Rating")
+                            Spacer()
+                            Text("\(String(song.rate))/5")
+                        }
                     }
-                    HStack {
-                        Text("Release date")
-                        Spacer()
-                        Text(song.releaseDate ?? Date(), style: .date)
-                    }
-                    HStack {
-                        Text("Rating")
-                        Spacer()
-                        Text("\(String(song.rate))/5")
-                    }
-                }
-                
-                Section(header: Text("Artist")) {
-                    HStack {
-                        Text("First name")
-                        Spacer()
-                        Text(song.artist?.firstName ?? "")
-                    }
-                    HStack {
-                        Text("Last name")
-                        Spacer()
-                        Text(song.artist?.lastName ?? "")
+                    
+                    Section(header: Text("Artist")) {
+                        HStack {
+                            Text("First name")
+                            Spacer()
+                            Text(song.artist?.firstName ?? "")
+                        }
+                        HStack {
+                            Text("Last name")
+                            Spacer()
+                            Text(song.artist?.lastName ?? "")
+                        }
                     }
                 }
             }
